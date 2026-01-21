0aa761674f8e8f01aa9ac818e037997712f574f5
YourName <your_email@example.com>
2026-01-21 10:44:56 +0700

    first commit

---
+++ test1.dart
@@ -0,0 +1,14 @@
+void main() {
+  print('Hello dart programing');
+  // การเขียน comment
+  /*
+  การ
+  คอมเมนต์
+  */
+  
+  // การ print แบบต่อข้อมความ
+  print('I lean '+' dart programming');
+  print("My name is prakadkeait\n i'm a devloper");
+  print ('''how to lern dart faster way in this class''');
+  print (r"Tihs in \n multiline \n string");
+}
